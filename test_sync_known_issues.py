7327b4f7b57beb9994e39cfa5d9920c023fb8749
Dan Rue <dan.rue@linaro.org>
2018-10-23 16:23:31 +0100

    Add a test to exercise some classes
    Specifically, SquadProject() and SquadKnownIssue()
    
    Signed-off-by: Dan Rue <dan.rue@linaro.org>

--- test_sync_known_issues.py
+++ test_sync_known_issues.py
@@ -52,3 +52,14 @@ def test_issue_url_note_null():
     is_equal = sync_known_issues.issues_equal(a, b)
     assert not is_equal
 
+def test_squad_known_issue_happy_path():
+    config_file = "test_data/test-issues.yaml"
+    config_data = sync_known_issues.parse_files([config_file])
+
+    a = sync_known_issues.SquadProject(config_data['LKFT-ltp-staging'])
+    assert a.projects == ['lkft/linux-mainline-oe',
+                          'lkft/linux-next-oe',
+                          'lkft/linux-stable-rc-4.17-oe']
+    assert a.environments == ['hi6220-hikey', 'juno-r2', 'x86']
+    assert len(a.known_issues) == 2
+
