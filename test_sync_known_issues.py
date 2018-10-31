63aad9a37523adb1c38a10119750b1bd660c725f
Dan Rue <dan.rue@linaro.org>
2018-10-31 15:50:00 -0500

    Add 'matrix_apply' option
    With this option, a list of projects+environments combinations can be
    specified. This allows the following definition:
    
      # Define a test with different combinations of environments and projects
      - test_name: ltp-syscalls-tests/fork10
        notes: 'LKFT: LTP: fork10: runs long and hangs machine on branches'
        url: https://bugs.linaro.org/show_bug.cgi?id=3719
        active: true
        matrix_apply: # Apply to hikey on all branches, and x86 on mainline.
          - environments:
            - hi6220-hikey
            projects: *projects_all
          - environments: *environments_all
            projects:
            - lkft/linux-mainline-oe
    
    Signed-off-by: Dan Rue <dan.rue@linaro.org>

--- test_sync_known_issues.py
+++ test_sync_known_issues.py
@@ -61,5 +61,34 @@ def test_squad_known_issue_happy_path():
                           'lkft/linux-next-oe',
                           'lkft/linux-stable-rc-4.17-oe']
     assert a.environments == ['hi6220-hikey', 'juno-r2', 'x86']
-    assert len(a.known_issues) == 5
+    assert len(a.known_issues) == 6
+
+#  - test_name: ltp-syscalls-tests/fork10
+#    notes: 'LKFT: LTP: fork10: runs long and hangs machine on branches'
+#    url: https://bugs.linaro.org/show_bug.cgi?id=3719
+#    active: true
+#    matrix_apply: # Apply to hikey on all branches, and x86 on mainline.
+#      - environments:
+#        - hi6220-hikey
+#        projects: *projects_all
+#      - environments: *environments_all
+#        projects:
+#        - lkft/linux-mainline-oe
+
+def test_squad_known_issue_happy_path():
+    config_file = "test_data/test-issues.yaml"
+    config_data = sync_known_issues.parse_files([config_file])
+
+    a = sync_known_issues.SquadProject(config_data['LKFT-ltp-staging'])
+    for known_issue in a.known_issues:
+        if known_issue.test_name == 'ltp-syscalls-tests/fork10':
+            assert known_issue.active
+            assert known_issue.intermittent is None
+            assert known_issue.projects_environments == {
+                'lkft/linux-mainline-oe': {'x86', 'hi6220-hikey', 'juno-r2'},
+                'lkft/linux-next-oe': {'hi6220-hikey'},
+                'lkft/linux-stable-rc-4.17-oe': {'hi6220-hikey'}
+            }
+
+
 
